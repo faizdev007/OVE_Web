d9273706ca922efd52078ef758120c0a699b3655
faizdev007 <faizdev007@gmail.com>
2025-08-14 13:18:41 +0530

    updat rating stars

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -318,7 +318,7 @@ export default function Hire()
                     <div className="h-20 aspect-[3/1] overflow-hidden">
                         <Image src={'/assets/google.webp'} alt="employee" width={500} height={500} className="mx-auto object-cover flex justify-center items-center w-32 aspect-[3/1] rounded-lg shadow-lg"/>
                         <div className="flex justify-center">
-                            <Rating rating={4.8}/>
+                            <Rating rating={4.5}/>
                         </div>
                     </div>
                     <TestimonialDev/>
