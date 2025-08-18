73364bb5756a74509bf0edd31cf2fc936bbaaeb7
faizdev007 <faizdev007@gmail.com>
2025-08-18 17:10:26 +0530

    update howitwork

--- components/HowItWorksSkillsPage.tsx
+++ components/HowItWorksSkillsPage.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image';
 export default function HowItWorks() {
   return (
     <main className="bg-black text-white">
-      <div className="mx-auto max-w-6xl px-4 py-10 md:py-14">
+      <div className="mx-auto container px-4 py-10 md:py-14">
         {/* Top intro block */}
-        <section className="grid grid-cols-1 sm:grid-cols-[250px_1fr] justify-center gap-5 items-start">
+        <section className="grid justify-center gap-5 items-start">
           {/* Avatar */}
-          <div className="flex sm:block justify-center">
+          <div className="hidden justify-center">
             <div className="relative w-full h-full aspect-[1/1]">
               <Image
                 src={'/assets/hire/howitwork.webp'} // put your image in public/person.jpg
@@ -22,8 +22,8 @@ export default function HowItWorks() {
           </div>
 
           {/* Text */}
-          <div className='flex flex-col justify-center h-full gap-2'>
-            <div className="flex items-center gap-2">
+          <div className='flex flex-col justify-center text-center h-full gap-2'>
+            <div className="flex items-center justify-center gap-2">
               <span className="h-1.5 w-5 rounded-full bg-[#2F6FFF]"></span>
               <span className="text-lg tracking-widest md:text-2xl text-white font-bold">HOW IT WORKS</span>
             </div>
@@ -36,7 +36,7 @@ export default function HowItWorks() {
         </section>
 
         {/* Big heading */}
-        <h3 className="mt-12 md:mt-16 text-xl md:text-2xl font-extrabold uppercase tracking-wide">
+        <h3 className="mt-12 md:mt-16 text-xl md:text-2xl text-center font-extrabold uppercase tracking-wide">
           How to hire a full stack developer through
           <br className="hidden md:block" />
           Optimal Virtual Employee
