19a8dc115579a21445b8b096716ff6c4ce0083a0
faizdev007 <faizdev007@gmail.com>
2025-09-08 14:13:44 +0530

    concent check before analitic process

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/footer";
 import CookieBanner from "@/components/CookieConsentBanner";
+import Analytics from "@/components/CookieConcentcheck";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,22 +38,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google Tag Manager Script for the head */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5K42N2M2');
-            `,
-          }}
-        />
+        <Analytics />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} opensans antialiased`}
