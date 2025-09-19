6fe4e6a71f42ce88779c4ddb4931354ac705dde9
faizdev007 <faizdev007@gmail.com>
2025-09-19 12:22:53 +0530

    updatge

--- app/(virtualAssistant)/services/page.tsx
+++ app/(virtualAssistant)/services/page.tsx
@@ -1,15 +1,3 @@
-import FaqSection from "@/components/main/Homepage/faq";
-import HeroSection from "@/components/virtualassistance/HeroSection";
-import ImagineTask from "@/components/virtualassistance/ImagineTask";
-import Modal from "@/components/virtualassistance/modal";
-import ServicessArea from "@/components/virtualassistance/ServicessArea";
-import SkillsVA from "@/components/virtualassistance/SkillsVA";
-import VACTA from "@/components/virtualassistance/VACTA";
-import VACTA2 from "@/components/virtualassistance/VACTA2";
-import VAStapes from "@/components/virtualassistance/VAStapes";
-import VAWork from "@/components/virtualassistance/VAWork";
-import ServiceWhyChooseUs from "@/components/virtualassistance/WhyChooseUs";
-import WhyYourShouldChoose from "@/components/virtualassistance/WhyYouShoudChoose";
 
 // src/app/(services)/services/page.tsx
 export default function ServicePage() {
@@ -28,22 +16,4 @@ export default function ServicePage() {
       </div>
     </section>
   );
-  // return (
-  //   <>
-  //     <HeroSection/>
-  //     <VAStapes/>
-  //     <VACTA/>
-  //     <ServicessArea/>
-  //     <ServiceWhyChooseUs/>
-  //     <ImagineTask/>
-  //     <ServicessArea/>
-  //     <VACTA/>
-  //     <SkillsVA/>
-  //     <WhyYourShouldChoose/>
-  //     <VAWork/>
-  //     <VACTA2/>
-  //     <FaqSection/>
-  //     <Modal isOpen={false} onClose={() => {}}/>
-  //   </>
-  // );
 }
