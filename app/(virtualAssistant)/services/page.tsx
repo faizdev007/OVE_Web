5f6c605be3bfe4621073b08b0854350757bd100a
faizdev007 <faizdev007@gmail.com>
2025-09-23 17:27:52 +0530

    update 404 page layout custome 404

--- app/(virtualAssistant)/services/page.tsx
+++ app/(virtualAssistant)/services/page.tsx
@@ -1,19 +1,7 @@
+import { notFound } from "next/navigation";
 
 // src/app/(services)/services/page.tsx
 export default function ServicePage() {
   // fallback (optional)
-  return (
-    <section className="flex items-center justify-center min-h-[60vh] bg-gray-100 text-white px-4">
-      <div className="text-center">
-        <h1 className="text-7xl font-extrabold text-red-500 tracking-widest">404</h1>
-        <p className="mt-4 text-xl text-black">Page not found</p>
-        <a
-          href="/"
-          className="mt-8 inline-block bg-oveblue hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition"
-        >
-          Back to Home
-        </a>
-      </div>
-    </section>
-  );
+  return notFound();
 }
